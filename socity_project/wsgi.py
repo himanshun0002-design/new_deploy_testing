339eb10ff50486e0526492a2df5b33c0d70ae62b
himanshu chauhan <himanshun00002@gmail.com>
2025-09-07 19:32:13 +0530

    Fix Render deployment configuration

--- socity_project/wsgi.py
+++ socity_project/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socity_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socity_project.settings_prod')
 
 application = get_wsgi_application()
+
+# For debugging purposes
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('', 8000, application)
+    print('Serving on port 8000...')
+    httpd.serve_forever()
